da69d803f99ca524e5cdbc168d09ac2903d69944
VicAbreuPro <victor.abreu09.pro@gmail.com>
2023-06-08 22:47:09 +0100

    Add firebase auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { AuthController } from './auth/AuthController/auth.controller';
+import { FirebaseAuthStrategy } from './auth/firebase/firebase-auth.strategy';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AuthController],
+  providers: [AppService, FirebaseAuthStrategy],
 })
 export class AppModule {}
