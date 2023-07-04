31ead64f52351dd73efa4d6b510b550f9faf4bba
VicAbreuPro <victor.abreu09.pro@gmail.com>
2023-07-05 00:42:58 +0100

    Add new request Dtos

--- src/auth/Dto/login.request.ts
+++ src/auth/Dto/login.request.ts
@@ -1,4 +1,17 @@
+import { IsNotEmpty, Matches } from 'class-validator'
+
 export class Login {
+    @IsNotEmpty({
+      message: 'Email is required!',
+    })
     email: string;
-    password: string
-}
+  
+    @IsNotEmpty({
+      message: 'Password is required!',
+    })
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+$/, {
+      message: 'Invalid Password, please reset it',
+    })
+    password: string;
+  }
+  
