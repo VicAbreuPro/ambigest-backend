548de35cebbe6ec3c599489a7b945f73263a1b44
VicAbreuPro <victor.abreu09.pro@gmail.com>
2023-07-06 13:28:46 +0100

    Refactor all module

--- src/EventsModule/Dtos/get-event-by-id.request.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty } from "class-validator";
-import { ObjectId } from "mongodb";
-
-export class GetEventById {
-
-    @IsNotEmpty()
-    readonly eventId: string
-}
---
+++ src/WasteCollection/Repository/waste-collection.repository.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@nestjs/common";
+import { DataSource, ObjectId } from "typeorm";
+import { WasteCollectionEntity } from "../Entities/waste-collection.entity";
+
+@Injectable()
+export class WasteCollectionRepository {
+    constructor(private dataSource: DataSource) {}
+
+    async getWasteCollection(id: ObjectId): Promise<WasteCollectionEntity> {
+        return await this.dataSource.manager.findOne(WasteCollectionEntity, {
+            select: ['type', "latitude", "longitude", "pickup_at", "time_of_day"],
+            where: {
+                _id: id
+            },
+        });
+    }
+
+    async getAllByUserId(userId: string): Promise<WasteCollectionEntity[]> {
+        return await this.dataSource.manager.find(WasteCollectionEntity, {
+            select: ['type', "latitude", "longitude", "pickup_at", "time_of_day"],
+            where: {
+                userId: userId
+            },
+        });
+    }
+
+    async upsertWasteCollection(entity: WasteCollectionEntity): Promise<WasteCollectionEntity> {
+        const result = await this.dataSource.manager.save(WasteCollectionEntity, entity);
+
+        return await this.dataSource.manager.findOne(WasteCollectionEntity, {
+            select: ['type', "latitude", "longitude", "pickup_at", "time_of_day"],
+            where: {
+                _id: result._id
+            }
+        });
+    }
+
+    async deleteWasteCollectionById(id: ObjectId) { 
+        return await this.dataSource.manager.delete(WasteCollectionEntity, id);
+    }
+}
