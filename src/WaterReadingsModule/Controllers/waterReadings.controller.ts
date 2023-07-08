d70ff53ef513bcce4e43b3e74c071b2255af1f47
VicAbreuPro <victor.abreu09.pro@gmail.com>
2023-07-09 00:39:10 +0100

    Drop the update water readings endpoint

--- src/WaterReadingsModule/Controllers/waterReadings.controller.ts
+++ src/WaterReadingsModule/Controllers/waterReadings.controller.ts
@@ -33,26 +33,6 @@ export class WaterReadingsController {
     }
   }
 
-  @Put('/amount')
-  @UseGuards(FirebaseAuthGuard)
-  @HttpCode(200)
-  async updateWaterReading(@Request() req: any, @Body() requestBody: UpdateReadingRequest): Promise<any> {
-    try{
-      return await this.waterReadingsService.updateReadingAmount(requestBody._id, req.user.uid, requestBody.amount);
-    
-    } catch(error){
-      if(error == 'Error: Water reading not found!'){
-        throw new HttpException('Water reading not found!', HttpStatus.BAD_REQUEST);
-      }
-
-      if(error == "BSONError: Argument passed in must be a string of 12 bytes or a string of 24 hex characters or an integer"){
-        throw new HttpException('Invalid ID!', HttpStatus.BAD_REQUEST);
-      }
-
-      throw new HttpException('Server error: ' + error, HttpStatus.SERVICE_UNAVAILABLE);
-    }
-  }
-
   @Delete('/')
   @UseGuards(FirebaseAuthGuard)
   @HttpCode(204)
